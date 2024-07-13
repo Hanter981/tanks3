fd8fae6eef1c797977162fd533d854bcc5ee8262
Hanter981 <vovachaban2008@ukr.net>
2024-07-13 11:51:19 +0300

    Add files via upload

---
+++ modules/classes.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+
+PATH = os.path.abspath(__file__ +'/..')
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 800
+STEP = 50
+
+
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Tanks 2D')
+
+class Block(pygame.Rect):
+    def __init__(self,x, y, type_block, image):
+        super().__init__(x, y, STEP, STEP)
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (STEP, STEP))
+        self.type_block = type_block
+    def blit(self):
+        window.flit(self.image, (self.x, self.y))
+class Bullet(pygame.Rect):
+    pass
+        
+class Panzar(pygame.Rect):
+    pass
+class Player(Panzar):
+    pass
+class Player2(Panzar):
+    pass
